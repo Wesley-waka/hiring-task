7c97197a8d013dd08361b27f56fd71fd63bbda26
wesley <wesleywaka77@gmail.com>
2025-02-02 14:09:18 +0300

    chore:clear sentiment controller as is a util

--- src/routers/sentiment.router.ts
+++
@@ -1,6 +0,0 @@
-import { sentimentHandler } from "@/services/sentiment.service";
-import { Router } from "express";
-
-export const sentimentRouter = Router();
-
-sentimentRouter.post('/analyze', sentimentHandler);
